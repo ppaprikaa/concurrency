6d5b65298e04a68acabbdffd15a83cd417383a6f
sanzhh <sanzhar.maratov@proton.me>
2023-09-26 17:20:05 +0600

    [added] ticket spinlock

---
+++ mutex-examples/spinlocks/ticket_spinlock.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <atomic>
+#include <cstdlib>
+#include <thread>
+
+class TicketLock
+{
+   using Ticket = size_t;
+   public:
+      void Lock()
+      {
+         Ticket my_ticket = next_free_ticket_.fetch_add(1);
+         while(owner_ticket_.load() != my_ticket)
+         {
+            // Back off
+         }
+      }
+
+      void Unlock()
+      {
+         owner_ticket_.fetch_add(1);
+      }
+   private:
+      std::atomic<Ticket> next_free_ticket_;
+      std::atomic<Ticket> owner_ticket_;
+};
