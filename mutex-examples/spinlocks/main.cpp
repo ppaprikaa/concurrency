902820cd01d9796b6d7f3796e6b0b88c8ce39396
ppaprikaa <sanzhar.maratov@proton.me>
2023-10-17 12:08:04 +0600

    better locks

--- mutex-examples/spinlocks/main.cpp
+++ mutex-examples/spinlocks/main.cpp
@@ -1,38 +1,20 @@
-#include <iostream>
-#include <thread>
+#include <mutex>
 #include <string>
+#include <thread>
 #include <vector>
 
-#include "ticket_spinlock.hpp"
+#include "Task.hpp"
 
-TicketLock mu;
+int main(int argc, char *argv[]) {
+  Task task;
 
-void DoWork()
-{
-   using namespace std::literals::chrono_literals;
-   mu.Lock();
-   std::vector<std::string> messages;
-   messages.push_back("operation 1: ");
-   messages.push_back("operation 2: ");
-   messages.push_back("operation 3: ");
+  std::thread tr1(task.DoJob());
+  std::thread tr2(task.DoJob());
+  std::thread tr3(task.DoJob());
 
-   for (auto msg : messages)
-   {
-      std::this_thread::sleep_for(1s);
-      std::cout << msg << std::this_thread::get_id() << std::endl;
-   }
-   mu.Unlock();
-}
+  tr1.join();
+  tr2.join();
+  tr3.join();
 
-int main(int argc, char * argv[])
-{
-   std::thread tr1(DoWork);
-   std::thread tr2(DoWork);
-   std::thread tr3(DoWork);
-
-   tr1.join();
-   tr2.join();
-   tr3.join();
-
-   return 0;
+  return 0;
 }
--- mutex-examples/spinlocks/ticket_spinlock.hpp
+++ mutex-examples/spinlocks/ticket_spinlock.hpp
@@ -1,26 +1,23 @@
 #pragma once
 #include <atomic>
 #include <cstdlib>
-#include <thread>
 
-class TicketLock
-{
-   using Ticket = size_t;
-   public:
-      void Lock()
-      {
-         Ticket my_ticket = next_free_ticket_.fetch_add(1);
-         while(owner_ticket_.load() != my_ticket)
-         {
-            // Back off
-         }
-      }
+class TicketLock {
+  using Ticket = size_t;
 
-      void Unlock()
-      {
-         owner_ticket_.fetch_add(1);
-      }
-   private:
-      std::atomic<Ticket> next_free_ticket_;
-      std::atomic<Ticket> owner_ticket_;
+public:
+  void Lock() {
+    Ticket my_ticket = next_free_ticket_.fetch_add(1);
+    while (owner_ticket_.load() != my_ticket) {
+      // Back off
+    }
+  }
+
+  void Unlock() { owner_ticket_.fetch_add(1); }
+
+  bool TryLock() { return false; }
+
+private:
+  std::atomic<Ticket> next_free_ticket_;
+  std::atomic<Ticket> owner_ticket_;
 };
